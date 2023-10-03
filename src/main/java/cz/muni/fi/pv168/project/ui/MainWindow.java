51e6a8e930e7dafb09e13300aa4130f016e4ce1f
Vojtěch Stárek <xstarek2@fi.muni.cz>
2023-10-03 15:31:33 +0200

    "Create" GUI outline

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/MainWindow.java
@@ -0,0 +1,109 @@
+package cz.muni.fi.pv168.project.ui;
+
+import cz.muni.fi.pv168.project.data.TestDataGenerator;
+import cz.muni.fi.pv168.project.model.Employee;
+import cz.muni.fi.pv168.project.model.Gender;
+import cz.muni.fi.pv168.project.ui.action.AddAction;
+import cz.muni.fi.pv168.project.ui.action.DeleteAction;
+import cz.muni.fi.pv168.project.ui.action.EditAction;
+import cz.muni.fi.pv168.project.ui.action.QuitAction;
+import cz.muni.fi.pv168.project.ui.model.EmployeeTableModel;
+import cz.muni.fi.pv168.project.ui.model.DepartmentListModel;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import java.awt.*;
+import java.util.List;
+
+public class MainWindow {
+
+    private final JFrame frame;
+
+    private final Action quitAction = new QuitAction();
+    private final Action addAction;
+    private final Action deleteAction;
+    private final Action editAction;
+
+    public MainWindow() {
+        frame = createFrame();
+
+        var testDataGenerator = new TestDataGenerator();
+        var employeeTable = createEmployeeTable(testDataGenerator.createTestEmployees(10));
+
+        var departmentListModel = new DepartmentListModel(testDataGenerator.getDepartments());
+        addAction = new AddAction(employeeTable, testDataGenerator, departmentListModel);
+        deleteAction = new DeleteAction(employeeTable);
+        editAction = new EditAction(employeeTable, departmentListModel);
+        employeeTable.setComponentPopupMenu(createEmployeeTablePopupMenu());
+        //frame.add(new JScrollPane(employeeTable), BorderLayout.CENTER);
+        frame.add(createTabbedPane(List.of(employeeTable,
+                createEmployeeTable(testDataGenerator.createTestEmployees(10)),
+                createEmployeeTable(testDataGenerator.createTestEmployees(10000)))), BorderLayout.CENTER);
+        frame.add(createToolbar(), BorderLayout.BEFORE_FIRST_LINE);
+        frame.setJMenuBar(createMenuBar());
+        frame.pack();
+    }
+
+    public void show() {
+        frame.setVisible(true);
+    }
+
+    private JFrame createFrame() {
+        var frame = new JFrame("Easy Food Recipe Book");
+        frame.setSize(new Dimension(1920, 1080));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return frame;
+    }
+
+    private JTable createEmployeeTable(List<Employee> employees) {
+        var model = new EmployeeTableModel(employees);
+        var table = new JTable(model);
+        table.setAutoCreateRowSorter(true);
+        table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
+        var genderComboBox = new JComboBox<>(Gender.values());
+        table.setDefaultEditor(Gender.class, new DefaultCellEditor(genderComboBox));
+        return table;
+    }
+
+    private JPopupMenu createEmployeeTablePopupMenu() {
+        var menu = new JPopupMenu();
+        menu.add(deleteAction);
+        menu.add(editAction);
+        return menu;
+    }
+
+    private JMenuBar createMenuBar() {
+        var menuBar = new JMenuBar();
+        var editMenu = new JMenu("Edit");
+        editMenu.setMnemonic('e');
+        editMenu.add(addAction);
+        editMenu.addSeparator();
+        editMenu.add(quitAction);
+        menuBar.add(editMenu);
+        return menuBar;
+    }
+
+    private JToolBar createToolbar() {
+        var toolbar = new JToolBar();
+        toolbar.add(quitAction);
+        toolbar.addSeparator();
+        return toolbar;
+    }
+
+    private JTabbedPane createTabbedPane(List<JTable> tables) {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        //ImageIcon icon = createImageIcon("images/middle.gif");
+
+        for (JTable table : tables) {
+            JScrollPane scrollPane = new JScrollPane(table);
+            tabbedPane.addTab("Random", null, scrollPane, "tooltip");
+        }
+
+        return tabbedPane;
+    }
+
+    private void rowSelectionChanged(ListSelectionEvent listSelectionEvent) {
+        var selectionModel = (ListSelectionModel) listSelectionEvent.getSource();
+        // here you can put the code for handling selection change
+    }
+}
