3f00e798dd64620df360c0058eb26376fe074218
Marek Eibel <536376@mail.muni.cz>
2023-10-07 18:27:24 +0200

    Implement basic design for RecipeTableComponent

--- src/main/java/cz/muni/fi/pv168/project/ui/model/RecipeTableComponent.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/RecipeTableComponent.java
@@ -1,24 +1,39 @@
 package cz.muni.fi.pv168.project.ui.model;
-
-import cz.muni.fi.pv168.project.model.Recipe;
+import cz.muni.fi.pv168.project.ui.resources.Icons;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 /**
+ * Represents RecipeTableComponent.
+ * This component servers as a structure to show basic information about the recipe
+ * like a name, short description or recipe categories.
+ *
  * @author Marek Eibel
  */
 public class RecipeTableComponent extends AbstractTableComponent {
 
-    public RecipeTableComponent(String title, String content) {
+    public RecipeTableComponent(String title, String content) { // TODO: add Recipe recipe as an argument here
+
         setLayout(new BorderLayout());
+
+        JPanel textPanel = new JPanel();
+        textPanel.setBackground(Color.orange);
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+        textPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
         JLabel titleLabel = new JLabel(title);
-        JLabel contentLabel = new JLabel(content);
+        JLabel textLabel = new JLabel(content);
+        textPanel.add(titleLabel);
+        textPanel.add(textLabel);
 
-        add(titleLabel, BorderLayout.NORTH);
-        add(contentLabel, BorderLayout.CENTER);
+        JPanel iconPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JLabel iconLabel = new JLabel(Icons.ADD_ICON);
 
-        setBorder(BorderFactory.createLineBorder(Color.BLACK)); // Optional: Add a border
+        iconPanel.add(iconLabel);
+
+        add(textPanel, BorderLayout.CENTER);
+        add(iconPanel, BorderLayout.EAST);
     }
-
 }
