1fa4e6841f63af921d556cdaab568d8e5db3c031
Marek Eibel <536376@mail.muni.cz>
2023-10-13 22:41:47 +0200

    Add override toString method for RecipeCategory

--- src/main/java/cz/muni/fi/pv168/project/model/RecipeCategory.java
+++ src/main/java/cz/muni/fi/pv168/project/model/RecipeCategory.java
@@ -42,4 +42,9 @@ public class RecipeCategory { // TODO probably must be reimplement it
     public void setCategoryColor(Color categoryColor) {
         this.categoryColor = categoryColor;
     }
+
+    @Override
+    public String toString() {
+        return getCategoryName();
+    }
 }
