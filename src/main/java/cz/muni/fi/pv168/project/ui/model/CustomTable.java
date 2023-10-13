89fcbf10fa15e27465e614ce148c5e90ea57fb66
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-13 20:03:51 +0200

    Edit CustomTable to support generics. Thanks to that, it can be now used to store Recipes, Custom units, Categories etc.

--- src/main/java/cz/muni/fi/pv168/project/ui/model/CustomTable.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/CustomTable.java
@@ -1,11 +1,16 @@
 package cz.muni.fi.pv168.project.ui.model;
 
+import cz.muni.fi.pv168.project.model.Employee;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class representing custom table where individual rows are represented by
@@ -13,41 +18,26 @@ import java.awt.event.MouseEvent;
  *
  * @author Marek Eibel
  */
-public class CustomTable extends JTable {
+public class CustomTable<T> extends JTable {
 
     private DefaultTableModel model;
     private final String name;
+
+    private TableCellEditor editor;
+    private TableCellRenderer renderer;
 
     /**
      * Creates new CustomTable.
      *
      * @param tableName name of the table
      */
-    public CustomTable(String tableName) {
+    public CustomTable(String tableName, TableCellEditor editor, TableCellRenderer renderer) {
 
         this.name = tableName;
+        this.editor = editor;
+        this.renderer = renderer;
         initModel();
         designTable();
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // Get the clicked point relative to the table
-                Point point = e.getPoint();
-
-                // Get the row and column indices
-                int clickedRow = rowAtPoint(point);
-                int clickedColumn = columnAtPoint(point);
-
-                // Do something with the clicked row and column
-                System.out.println("Clicked on Row: " + clickedRow + ", Column: " + clickedColumn);
-
-                // Example: If you want to get the value in the clicked cell
-                RecipeTableComponent value = (RecipeTableComponent) getValueAt(clickedRow, clickedColumn);
-                if (value != null) {
-                    System.out.println("Recipe name: " + value.getRecipe().getName());
-                }
-            }
-        });
     }
 
     private void designTable() {
@@ -67,43 +57,19 @@ public class CustomTable extends JTable {
         model = new DefaultTableModel(INITIAL_ROWS_NUMBER, INITIAL_COLUMNS_NUMBER);
         model.setColumnIdentifiers(new String[]{name});
         setModel(model);
-        this.setCellEditor(new CustomCellEditor());
-        getColumnModel().getColumn(0).setCellRenderer(new CustomCellRenderer());
+        getColumnModel().getColumn(0).setCellEditor(editor);
+        getColumnModel().getColumn(0).setCellRenderer(renderer);
     }
 
     /**
-     * Adds component to the table.
+     * Adds data to the table.
      *
-     * @param component component to add
+     * @param data row to add
      */
-    public void addComponent(AbstractTableComponent component) {
-
-        model.addRow(new AbstractTableComponent[]{component});
-    }
-
-    private static class CustomCellRenderer extends JPanel implements TableCellRenderer {
-        public CustomCellRenderer() {
-            setLayout(new BorderLayout());
-        }
-
-        @Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-
-            if (value instanceof AbstractTableComponent customComponent) {
-
-                removeAll();
-
-                JPanel paddedComponent = new JPanel(new BorderLayout());
-                paddedComponent.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-                paddedComponent.add(customComponent, BorderLayout.CENTER);
-
-                add(paddedComponent, BorderLayout.CENTER);
-
-                return this;
-            }
-
-            return null;
-        }
+    public void addData(T data) {
+        List<T> lst = new ArrayList<>();
+        lst.add(data);
+        model.addRow(lst.toArray());
     }
 
     /**
