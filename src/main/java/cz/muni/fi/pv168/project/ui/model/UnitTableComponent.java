eb2b46fbbe5a656ecb75ca6253929bb7de71fc1e
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-15 19:01:15 +0200

    Make UnitTableComponent

--- src/main/java/cz/muni/fi/pv168/project/ui/model/UnitTableComponent.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/UnitTableComponent.java
@@ -2,10 +2,27 @@ package cz.muni.fi.pv168.project.ui.model;
 
 import cz.muni.fi.pv168.project.model.Unit;
 
+import javax.swing.*;
+import java.awt.*;
+
+// TODO better design
 public class UnitTableComponent extends AbstractTableComponent {
     private Unit unit;
 
     public UnitTableComponent(Unit unit) {
         this.unit = unit;
+
+        setBackground(Color.orange);
+        //var bl = new BoxLayout(this, BoxLayout.X_AXIS);
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        JLabel nameLabel = new JLabel(unit.getName());
+        add(nameLabel);
+        JLabel conversionUnitLabel = new JLabel(unit.getConversionUnit() != null ? unit.getConversionUnit().getName() : "Base unit");
+        add(conversionUnitLabel);
+
+        if (unit.getConversionUnit() != null) {
+            JLabel ratioLabel = new JLabel("1 " + unit.getName() + " = " + unit.getConversionUnit().getRatio());
+            add(ratioLabel);
+        }
     }
 }
