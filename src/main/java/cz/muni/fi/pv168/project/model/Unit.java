a4f6a59ceeaf2e7b14ea1e84cc5da26b707a43dc
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-15 19:00:17 +0200

    Edit Unit model to support Unit conversion

--- src/main/java/cz/muni/fi/pv168/project/model/Unit.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Unit.java
@@ -7,29 +7,55 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO conversion inbetween standardized units and our custom units
+
 public class Unit extends AbstractUserItemData {
     private String name;
     private final int id;
     private static int idCounter = 0;
+    private Unit conversionUnit;
+    private int ratio;
 
     public static List<Unit> listOfUnits;
 
     static {
         listOfUnits = new ArrayList<>(
-                List.of(new Unit("Kg"),
-                        new Unit("Litre")
+                List.of(new Unit("gram", null, -1),
+                        new Unit("millilitre", null, -1)
                 ));
+        listOfUnits.add(new Unit("kilogram", listOfUnits.get(0), 1000));
+        listOfUnits.add(new Unit("litre", listOfUnits.get(1), 1000));
     }
 
-    public Unit(String name) {
+    public Unit(String name, Unit conversionUnit, int ratio) {
         this.name = name;
         this.id = Unit.idCounter++;
+        this.conversionUnit = conversionUnit;
+        this.ratio = ratio;
 //        listOfUnits.add(this);
     }
 
     public String getName() {
         return name;
+    }
+
+    public Unit getConversionUnit() {
+        return conversionUnit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setConversionUnit(Unit conversionUnit) {
+        this.conversionUnit = conversionUnit;
+    }
+
+    public void setRatio(int ratio) {
+        this.ratio = ratio;
+    }
+
+    public int getRatio() {
+        return ratio;
     }
 
     public int getUnitId() {
