3d2b72422a2b02928e665d9de01f28b71a5f7b22
Marek Eibel <536376@mail.muni.cz>
2023-10-24 15:42:49 +0200

    Implement only basic structure for Range class

---
+++ src/main/java/cz/muni/fi/pv168/project/model/Range.java
@@ -0,0 +1,49 @@
+package cz.muni.fi.pv168.project.model;
+
+/**
+ * @author Marek Eibel
+ */
+public class Range {
+
+    private int min;
+    private int max;
+    private int value;
+
+    /**
+     * Create Range object holding `value` in range [minimalValue, maximalValue].
+     *
+     * @param minimalValue
+     * @param maximalValue
+     */
+    Range(int minimalValue, int maximalValue, int value) {
+        this.min = minimalValue;
+        this.max = maximalValue;
+
+        // TODO make check here
+        this.value = value;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
