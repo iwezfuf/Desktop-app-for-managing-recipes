4fbfcde799c1c41aff07522f65caef946ac756cd
Martin Drazkovec <536686@mail.muni.cz>
2023-10-25 14:42:55 +0200

    add importDialog and ImportType

--- src/main/java/cz/muni/fi/pv168/project/ui/action/ImportAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/ImportAction.java
@@ -1,5 +1,7 @@
 package cz.muni.fi.pv168.project.ui.action;
 
+import cz.muni.fi.pv168.project.model.ImportType;
+import cz.muni.fi.pv168.project.ui.dialog.ImportDialog;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
 import javax.swing.AbstractAction;
@@ -12,9 +14,16 @@ public final class ImportAction extends AbstractAction {
         super("Import", Icons.IMPORT_ICON);
         putValue(SHORT_DESCRIPTION, "Imports data from file");
 //        putValue(MNEMONIC_KEY, KeyEvent.VK_Q);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        ImportDialog dialog = new ImportDialog(null);
+        dialog.setVisible(true);
+    }
+
+    public static void importData(ImportType importType, String selectedFile) {
+        System.out.println("import from file: " + selectedFile + " with type: " + importType);
     }
 }
