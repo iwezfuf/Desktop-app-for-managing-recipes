f387c2bf6b6ce5c8ba33c402d249cee172d9c455
Marek Eibel <536376@mail.muni.cz>
2023-10-25 22:37:05 +0200

    Implement CancelFilterAction to cancel filter recipes

--- src/main/java/cz/muni/fi/pv168/project/ui/action/CancelFilterAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/CancelFilterAction.java
@@ -1,18 +1,25 @@
 package cz.muni.fi.pv168.project.ui.action;
 
+import cz.muni.fi.pv168.project.model.AbstractUserItemData;
+import cz.muni.fi.pv168.project.ui.model.CustomTable;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class CancelFilterAction extends AbstractAction {
-    public CancelFilterAction() {
-        super("Cancel Filter", Icons.CANCEL_FILTER_ICON);
+
+    private CustomTable table;
+    private String name;
+
+    public CancelFilterAction(String name, CustomTable<? extends AbstractUserItemData> table) {
+        super("Cancel " + name + " Filter", Icons.CANCEL_FILTER_ICON);
         putValue(SHORT_DESCRIPTION, "Cancels filter");
+        this.table = table;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        this.table.cancelFilter();
     }
 }
