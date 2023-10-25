c9b740d4272208ce9c439f0d99b5135d371b802b
Martin Drazkovec <536686@mail.muni.cz>
2023-10-25 14:54:26 +0200

    open file browser when export

--- src/main/java/cz/muni/fi/pv168/project/ui/action/ExportAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/ExportAction.java
@@ -1,10 +1,13 @@
 package cz.muni.fi.pv168.project.ui.action;
 
+import cz.muni.fi.pv168.project.ui.dialog.ImportDialog;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
-import javax.swing.AbstractAction;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public final class ExportAction extends AbstractAction {
 
@@ -16,5 +19,18 @@ public final class ExportAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON files", "json");
+        fileChooser.setFileFilter(filter);
+
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            exportData(selectedFile.getAbsolutePath());
+        }
+    }
+    
+    private static void exportData(String selectedFile) {
+        System.out.println("export to file: " + selectedFile);
     }
 }
