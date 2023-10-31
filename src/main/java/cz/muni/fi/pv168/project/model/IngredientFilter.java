40dd103e3a51f196e780bbc151537eecd4efd08a
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-31 21:28:15 +0100

    Add IngredientFilter

---
+++ src/main/java/cz/muni/fi/pv168/project/model/IngredientFilter.java
@@ -0,0 +1,33 @@
+package cz.muni.fi.pv168.project.model;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class IngredientFilter implements AbstractFilter{
+
+    private Range nutritionValueRange;
+
+    public IngredientFilter(Range nutritionValueRange) {
+        this.nutritionValueRange = nutritionValueRange;
+    }
+
+    private boolean evaluateIngredient(Ingredient ingredient) {
+        return nutritionValueRange.isValueInRange(ingredient.getNutritionalValue());
+    }
+
+    public Range getNutritionValueRange() {
+        return nutritionValueRange;
+    }
+
+    @Override
+    public RowFilter<DefaultTableModel, Object> getRowFilter() {
+        return new RowFilter<DefaultTableModel, Object>() {
+            @Override
+            public boolean include(Entry<? extends DefaultTableModel, ?> entry) {
+
+                Object columnValue = entry.getValue(0);
+                return evaluateIngredient((Ingredient)columnValue);
+            }
+        };
+    }
+}
