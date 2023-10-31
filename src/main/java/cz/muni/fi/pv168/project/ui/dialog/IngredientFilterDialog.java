79c7a9a3cd5c4f5143db251d15ba03ec9dbfb5ec
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-31 21:28:24 +0100

    Add IngredientFilterDialog

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/IngredientFilterDialog.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pv168.project.ui.dialog;
+
+import cz.muni.fi.pv168.project.model.AbstractFilter;
+import cz.muni.fi.pv168.project.model.IngredientFilter;
+import cz.muni.fi.pv168.project.model.Range;
+
+import java.awt.*;
+
+public class IngredientFilterDialog extends EntityDialog<IngredientFilter> {
+
+    private final Range nutritionalRange = new Range(0, 1000000000);
+    private final RangePanel nutritionalValuePanel = new RangePanel(nutritionalRange, "Filter ingredients by their nutritional value", "kcal");
+
+    public IngredientFilterDialog(AbstractFilter filter) {
+        super(new Dimension(800, 600));
+        this.add(nutritionalValuePanel);
+        fillFilter(filter);
+    }
+
+    private void fillFilter(AbstractFilter filter) {
+        if (filter == null) {
+            return;
+        }
+        IngredientFilter inf = (IngredientFilter) filter;
+        nutritionalValuePanel.fillRange(nutritionalRange);
+        nutritionalRange.setMin(inf.getNutritionValueRange().getMin());
+        nutritionalRange.setMax(inf.getNutritionValueRange().getMax());
+    }
+
+    @Override
+    IngredientFilter getEntity() {
+        return new IngredientFilter(nutritionalRange);
+    }
+}
