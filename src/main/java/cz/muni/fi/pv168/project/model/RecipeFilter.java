5308c9fb35f824634d7345a6a3daf02ae1297f4a
Ondřej Marek <xmarek9@fi.muni.cz>
2023-10-31 21:24:42 +0100

    Edit RecipeFilter to correctly evaluate filter conditions.

--- src/main/java/cz/muni/fi/pv168/project/model/RecipeFilter.java
+++ src/main/java/cz/muni/fi/pv168/project/model/RecipeFilter.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pv168.project.model;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.util.List;
+import java.util.Set;
 
 /**
  * The RecipeFilter class represents a filter for recipes.
@@ -14,8 +15,8 @@ import java.util.List;
  */
 public class RecipeFilter implements AbstractFilter {
 
-    private List<Ingredient> ingredientsInFilter;
-    private List<RecipeCategory> recipeCategoriesInFilter;
+    private Set<Ingredient> ingredientsInFilter;
+    private Set<RecipeCategory> recipeCategoriesInFilter;
     private Range nutritionValueRange;
     private Range preperationTimeRange;
 
@@ -29,7 +30,7 @@ public class RecipeFilter implements AbstractFilter {
      * @param nutritionValueRange range of nutritional values to filter recipes by
      * @param preperationTimeRange range of preparation times to filter recipes by
      */
-    public RecipeFilter(List<Ingredient> ingredientsInFilter, List<RecipeCategory> recipeCategoriesInFilter, Range nutritionValueRange, Range preperationTimeRange) {
+    public RecipeFilter(Set<Ingredient> ingredientsInFilter, Set<RecipeCategory> recipeCategoriesInFilter, Range nutritionValueRange, Range preperationTimeRange) {
         this.ingredientsInFilter = ingredientsInFilter;
         this.recipeCategoriesInFilter = recipeCategoriesInFilter;
         this.nutritionValueRange = nutritionValueRange;
@@ -45,17 +46,40 @@ public class RecipeFilter implements AbstractFilter {
      * @return true if the given recipe should be left in the table else false
      */
     private boolean evaluateRecipe(Recipe recipe) {
+        return recipe.getIngredientsSet().containsAll(ingredientsInFilter) && evaluateRecipeCategoriesFilter(recipe)
+                && preperationTimeRange.isValueInRange(recipe.getPreparationTime()) &&
+                nutritionValueRange.isValueInRange(recipe.getNutritionalValue());
+    }
 
-        return preperationTimeRange.isValueInRange(recipe.getPreparationTime()) &&
-                nutritionValueRange.isValueInRange(recipe.getNutritionalValue()); // TODO: check also ingredients and categories
-                                                                                  // TODO: if the object is null, then do not check it
+    private boolean evaluateRecipeCategoriesFilter(Recipe recipe) {
+        if (recipeCategoriesInFilter.isEmpty()) {
+            return true;
+        }
+
+        return recipeCategoriesInFilter.contains(recipe.getCategory());
+    }
+
+    public Set<Ingredient> getIngredientsInFilter() {
+        return ingredientsInFilter;
+    }
+
+    public Set<RecipeCategory> getRecipeCategoriesInFilter() {
+        return recipeCategoriesInFilter;
+    }
+
+    public Range getNutritionValueRange() {
+        return nutritionValueRange;
+    }
+
+    public Range getPreperationTimeRange() {
+        return preperationTimeRange;
     }
 
     @Override
     public RowFilter<DefaultTableModel, Object> getRowFilter() {
         return new RowFilter<DefaultTableModel, Object>() {
             @Override
-            public boolean include(Entry<? extends DefaultTableModel, ? extends Object> entry) {
+            public boolean include(Entry<? extends DefaultTableModel, ?> entry) {
 
                 Object columnValue = entry.getValue(0); // TODO: think about where to set this column index
                 return evaluateRecipe((Recipe)columnValue);
