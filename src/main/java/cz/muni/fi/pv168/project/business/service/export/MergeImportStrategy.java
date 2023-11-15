b1dfa49e5449b053d7b7ca0aaf821648a68ef087
Marek Eibel <536376@mail.muni.cz>
2023-11-15 21:39:04 +0100

    Implement MergeImportStrategy

---
+++ src/main/java/cz/muni/fi/pv168/project/business/service/export/MergeImportStrategy.java
@@ -0,0 +1,62 @@
+package cz.muni.fi.pv168.project.business.service.export;
+
+import cz.muni.fi.pv168.project.business.model.Ingredient;
+import cz.muni.fi.pv168.project.business.model.Recipe;
+import cz.muni.fi.pv168.project.business.model.RecipeCategory;
+import cz.muni.fi.pv168.project.business.model.Unit;
+import cz.muni.fi.pv168.project.business.service.crud.CrudService;
+import cz.muni.fi.pv168.project.business.service.export.batch.BatchImporter;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents merge import strategy. Duplicate of units & ingredients will not be stored.
+ *
+ * @author Marek Eibel
+ */
+public class MergeImportStrategy implements ImportStrategy {
+
+    @Override
+    public void executeImport(CrudService<Recipe> recipeCrudService, CrudService<Ingredient> ingredientCrudService,
+                              CrudService<Unit> unitCrudService, CrudService<RecipeCategory> recipeCategoryCrudService,
+                              BatchImporter batchImporter, String filePath) {
+
+        Recipe.getListOfRecipes().clear();
+        Ingredient.getListOfIngredients().clear();
+        RecipeCategory.getListOfCategories().clear();
+        Unit.getListOfUnits().clear();
+
+        var batch = batchImporter.importBatch(filePath);
+
+        batch.recipes().forEach(recipe -> ImportStrategy.createRecipe(recipe, recipeCrudService));
+        batch.ingredients().forEach(ingredient -> createIngredient(ingredient, ingredientCrudService));
+        batch.units().forEach(unit -> createUnit(unit, unitCrudService));
+        batch.recipeCategories().forEach(recipeCategory -> ImportStrategy.createRecipeCategory(recipeCategory, recipeCategoryCrudService));
+    }
+
+    private static void createIngredient(Ingredient ingredient, CrudService<Ingredient> ingredientCrudService) { // TODO: create dialog window with all duplicates where the user has a chance which version should be used
+        List<Ingredient> ingredientList = ingredientCrudService.findAll();
+        for (Ingredient ingredientInList : ingredientList) {
+            if (!Objects.equals(ingredientInList.getName(), ingredient.getName())) {
+                new JOptionPane().showMessageDialog(null, "Unable to merge duplicate ingredient: " + ingredient, "Error", JOptionPane.ERROR_MESSAGE);
+                break;
+            } else {
+                ingredientCrudService.create(ingredient).intoException();
+            }
+        }
+    }
+
+    private static void createUnit(Unit unit, CrudService<Unit> unitCrudService) {
+        List<Unit> unitList = unitCrudService.findAll();
+        for (Unit unitInList : unitList) {
+            if (!Objects.equals(unitInList.getName(), unit.getName())) {
+                new JOptionPane().showMessageDialog(null, "Unable to merge duplicate unit: " + unit, "Error", JOptionPane.ERROR_MESSAGE);
+                break;
+            } else {
+                unitCrudService.create(unit).intoException();
+            }
+        }
+    }
+}
