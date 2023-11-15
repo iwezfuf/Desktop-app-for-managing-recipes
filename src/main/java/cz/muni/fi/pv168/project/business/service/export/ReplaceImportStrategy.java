3a6daa5e4e7e4cfe4d7e271b5fe867bf44ec29c5
Marek Eibel <536376@mail.muni.cz>
2023-11-15 21:39:12 +0100

    Implement ReplaceImportStrategy

---
+++ src/main/java/cz/muni/fi/pv168/project/business/service/export/ReplaceImportStrategy.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pv168.project.business.service.export;
+
+import cz.muni.fi.pv168.project.business.model.Ingredient;
+import cz.muni.fi.pv168.project.business.model.Recipe;
+import cz.muni.fi.pv168.project.business.model.RecipeCategory;
+import cz.muni.fi.pv168.project.business.model.Unit;
+import cz.muni.fi.pv168.project.business.service.crud.CrudService;
+import cz.muni.fi.pv168.project.business.service.export.batch.BatchImporter;
+
+/**
+ * @author Marek Eibel
+ */
+public class ReplaceImportStrategy implements ImportStrategy {
+    @Override
+    public void executeImport(CrudService<Recipe> recipeCrudService, CrudService<Ingredient> ingredientCrudService,
+                              CrudService<Unit> unitCrudService, CrudService<RecipeCategory> recipeCategoryCrudService,
+                              BatchImporter batchImporter, String filePath) {
+
+        Recipe.getListOfRecipes().clear();
+        Ingredient.getListOfIngredients().clear();
+        RecipeCategory.getListOfCategories().clear();
+        Unit.getListOfUnits().clear();
+
+        ingredientCrudService.deleteAll();
+        recipeCrudService.deleteAll();
+        recipeCategoryCrudService.deleteAll();
+        unitCrudService.deleteAll();
+
+        var batch = batchImporter.importBatch(filePath);
+
+        batch.recipes().forEach(recipe -> ImportStrategy.createRecipe(recipe, recipeCrudService));
+        batch.ingredients().forEach(ingredient -> ImportStrategy.createIngredient(ingredient, ingredientCrudService));
+        batch.units().forEach(unit -> ImportStrategy.createUnit(unit, unitCrudService));
+        batch.recipeCategories().forEach(recipeCategory -> ImportStrategy.createRecipeCategory(recipeCategory, recipeCategoryCrudService));
+    }
+}
