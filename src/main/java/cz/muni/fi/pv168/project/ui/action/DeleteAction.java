536bcac3f05a9af795b26ec6a74921e421ac4306
Martin Drazkovec <xdrazko1@fi.muni.cz>
2023-11-16 10:29:39 +0100

    generalize DeleteAction

--- src/main/java/cz/muni/fi/pv168/project/ui/action/DeleteAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/DeleteAction.java
@@ -13,22 +13,22 @@ import java.util.Comparator;
 
 public final class DeleteAction extends AbstractAction {
 
-    private final JTable employeeTable;
+    private final JTable entityTable;
 
-    public DeleteAction(JTable employeeTable) {
+    public DeleteAction(JTable entityTable) {
         super("Delete", Icons.DELETE_ICON);
-        this.employeeTable = employeeTable;
-        putValue(SHORT_DESCRIPTION, "Deletes selected employees");
+        this.entityTable = entityTable;
+        putValue(SHORT_DESCRIPTION, "Deletes selected entities");
         putValue(MNEMONIC_KEY, KeyEvent.VK_D);
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl D"));
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        var employeeTableModel = (EntityTableModel) employeeTable.getModel();
-        Arrays.stream(employeeTable.getSelectedRows())
+        var employeeTableModel = (EntityTableModel) entityTable.getModel();
+        Arrays.stream(entityTable.getSelectedRows())
                 // view row index must be converted to model row index
-                .map(employeeTable::convertRowIndexToModel)
+                .map(entityTable::convertRowIndexToModel)
                 .boxed()
                 // We need to delete rows in descending order to not change index of rows
                 // which are not deleted yet
