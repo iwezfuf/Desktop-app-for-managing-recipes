607f61a8e70a9c0a8262d868e44ce9cf0ef97498
Marek Eibel <536376@mail.muni.cz>
2023-11-17 21:54:36 +0100

    Database and RecipeDialog changes

--- src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
+++ src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
@@ -104,35 +104,37 @@ CREATE TABLE IF NOT EXISTS "RecipeIngredientAmount"
 --
 -- // TODO: remove this later
 
---  Departments
-INSERT INTO "Department" ("guid", "number", "name")
-VALUES ('d-1', '007', 'IT'),
-       ('d-2', '666', 'Sales'),
-       ('d-3', '112', 'HR')
-;
+----  Departments
+--INSERT INTO "Department" ("guid", "number", "name")
+--VALUES ('d-1', '007', 'IT'),
+--       ('d-2', '666', 'Sales'),
+--       ('d-3', '112', 'HR')
+--;
 
---  Employees
-INSERT INTO "Employee" ("guid", "firstName", "lastName", "birthDate", "gender", "departmentId")
-VALUES ('martin', 'martin', 'drazkovec', CURRENT_TIMESTAMP, 'MALE', '1')
-;
+----  Employees
+--INSERT INTO "Employee" ("guid", "firstName", "lastName", "birthDate", "gender", "departmentId")
+--VALUES ('martin', 'martin', 'drazkovec', CURRENT_TIMESTAMP, 'MALE', '1')
+--;
 
---  RecipeCategories
-INSERT INTO "RecipeCategory" ("guid", "name", "color")
-VALUES ('lunch', 'lunch', '1'),
-       ('dinner', 'dinner', '2')
-;
-
---  Recipes
-INSERT INTO "Recipe" ("guid", "name", "description", "preparationTime", "numOfServings", "instructions", "recipeCategoryId")
-VALUES ('pancakes', 'pancakes', 'delicious pancakes', '10', '2', 'mix everything together', '1')
-;
-
-INSERT INTO "Recipe" ("guid", "name", "description", "preparationTime", "numOfServings", "instructions", "recipeCategoryId")
-VALUES ('aaa', 'zzz', 'aaa', '15', '5', 'aaaaaaa', '1')
-;
+----  RecipeCategories
+--INSERT INTO "RecipeCategory" ("guid", "name", "color")
+--VALUES ('lunch', 'lunch', '1'),
+--       ('dinner', 'dinner', '2')
+--;
+--
+----  Recipes
+--INSERT INTO "Recipe" ("guid", "name", "description", "preparationTime", "numOfServings", "instructions", "recipeCategoryId")
+--VALUES ('pancakes', 'pancakes', 'delicious pancakes', '10', '2', 'mix everything together', '1')
+--;
+--
+--INSERT INTO "Recipe" ("guid", "name", "description", "preparationTime", "numOfServings", "instructions", "recipeCategoryId")
+--VALUES ('aaa', 'zzz', 'aaa', '15', '5', 'aaaaaaa', '1')
+--;
 
 --  Units
--- TODO this is probably not the best way to do this...
+-- TODO: move to somewhere else
 INSERT INTO "Unit" ("guid", "name", "abbreviation", "conversionRatio", "conversionUnitId")
-VALUES ('Base Unit', 'Base Unit', 'bu', '1', NULL)
-;
+SELECT 'Base Unit', 'Base Unit', 'bu', '1', NULL
+WHERE NOT EXISTS (
+    SELECT 1 FROM "Unit" WHERE "guid" = 'Base Unit'
+);
