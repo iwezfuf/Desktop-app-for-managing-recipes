5f1dd20c027f25a8d4fbb4c39f15af05e2cfa44c
Marek Eibel <536376@mail.muni.cz>
2023-11-19 14:54:24 +0100

    Rendering of colorful rows inside RecipeCategoryTablePanel

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/RecipeCategoryTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/RecipeCategoryTablePanel.java
@@ -6,6 +6,8 @@ import cz.muni.fi.pv168.project.ui.dialog.EntityDialog;
 import cz.muni.fi.pv168.project.ui.model.EntityTableModel;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.*;
 import java.util.function.Consumer;
 
 public class RecipeCategoryTablePanel extends EntityTablePanel<RecipeCategory> {
@@ -17,11 +19,24 @@ public class RecipeCategoryTablePanel extends EntityTablePanel<RecipeCategory> {
     protected JTable setUpTable(EntityTableModel<RecipeCategory> entityTableModel) {
         var table = new JTable(entityTableModel);
         table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
+        table.setDefaultRenderer(Object.class, new ColoredRowRenderer());
         return table;
     }
 
     @Override
     protected JPanel setUpTableWithSidePanel(EntityTableModel<RecipeCategory> entityTableModel, int frameHeight) {
         return null;
+    }
+
+    private static class ColoredRowRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            Component rendererComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            Color color = (Color) table.getValueAt(row, 1);
+            rendererComponent.setBackground(color);
+
+            return rendererComponent;
+        }
     }
 }
