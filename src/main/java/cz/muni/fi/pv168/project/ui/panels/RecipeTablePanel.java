40bac6138cf90317899cdd32a06593a1dacdd2d3
Marek Eibel <536376@mail.muni.cz>
2023-11-19 14:44:32 +0100

    Rendering of colorful rows inside RecipeTable

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/RecipeTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/RecipeTablePanel.java
@@ -1,11 +1,13 @@
 package cz.muni.fi.pv168.project.ui.panels;
 
 import cz.muni.fi.pv168.project.business.model.Recipe;
+import cz.muni.fi.pv168.project.business.model.RecipeCategory;
 import cz.muni.fi.pv168.project.business.service.validation.Validator;
 import cz.muni.fi.pv168.project.ui.dialog.EntityDialog;
 import cz.muni.fi.pv168.project.ui.model.EntityTableModel;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 import java.util.function.Consumer;
 
@@ -28,6 +30,7 @@ public class RecipeTablePanel extends EntityTablePanel<Recipe> {
         mainPanel.add(sidePanel, BorderLayout.WEST);
         var table = new JTable(entityTableModel);
         table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
+        table.setDefaultRenderer(Object.class, new ColoredRowRenderer());
         mainPanel.add(new JScrollPane(table), BorderLayout.CENTER);
         return mainPanel;
     }
@@ -37,5 +40,16 @@ public class RecipeTablePanel extends EntityTablePanel<Recipe> {
         JPanel sidePanel = new JPanel();
         sidePanel.setPreferredSize(new Dimension(175, frameHeight));
         return sidePanel;
+    }
+
+    private static class ColoredRowRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            Component rendererComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            RecipeCategory recipeCategory = (RecipeCategory) table.getValueAt(row, 4);
+            rendererComponent.setBackground(recipeCategory.getColor());
+            return rendererComponent;
+        }
     }
 }
