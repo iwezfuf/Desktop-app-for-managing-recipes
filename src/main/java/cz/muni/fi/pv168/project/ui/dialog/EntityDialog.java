2dab654983618482a456e33835e53f64eb44da89
Marek Eibel <536376@mail.muni.cz>
2023-11-24 21:53:18 +0100

    EntityDialog can handle null values from dialogs

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/EntityDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/EntityDialog.java
@@ -43,14 +43,22 @@ public abstract class EntityDialog<E> {
     abstract E getEntity();
 
     public Optional<E> show(JComponent parentComponent, String title) {
+
         int result = JOptionPane.showOptionDialog(parentComponent, panel, title,
                 OK_CANCEL_OPTION, PLAIN_MESSAGE, null, null, null);
 
-        // TODO implement validator
         if (result == OK_OPTION) {
-            return Optional.of(getEntity());
+
+            var entity = getEntity();
+
+            if (entity != null) { // if the entity is null, it signals that user has set wrong data in the dialog
+                return Optional.of(entity);
+            } else {
+                return Optional.empty();
+            }
         } else {
             return Optional.empty();
         }
+
     }
 }
