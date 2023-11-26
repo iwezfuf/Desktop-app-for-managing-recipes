7937cfa490b68589bbf1a3e0a20a19445c7243a3
Ondřej Marek <xmarek9@fi.muni.cz>
2023-11-26 15:08:15 +0100

    Add ingredient side filter panel

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/IngredientTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/IngredientTablePanel.java
@@ -5,14 +5,21 @@ import cz.muni.fi.pv168.project.business.service.validation.Validator;
 import cz.muni.fi.pv168.project.ui.dialog.EntityDialog;
 import cz.muni.fi.pv168.project.ui.model.EntityTableModel;
 import cz.muni.fi.pv168.project.ui.model.EntityTableModelProvider;
+import cz.muni.fi.pv168.project.ui.panels.filter.IngredientFilterPanel;
+import cz.muni.fi.pv168.project.ui.panels.filter.RecipeFilterPanel;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.function.Consumer;
 
 public class IngredientTablePanel extends EntityTablePanelS<Ingredient> {
+
+    private IngredientFilterPanel ingredientFilterPanel;
     public IngredientTablePanel(EntityTableModel<Ingredient> entityTableModel, Validator<Ingredient> ingredientValidator, Class<? extends EntityDialog<Ingredient>> ingredientDialog, Consumer<Integer> onSelectionChange, EntityTableModelProvider etmp) {
         super(entityTableModel, Ingredient.class, ingredientValidator, ingredientDialog, onSelectionChange, etmp);
+
+        this.ingredientFilterPanel = new IngredientFilterPanel(this);
+        this.getSideScrollPane().setViewportView(ingredientFilterPanel);
     }
 
 }
---
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/filter/IngredientFilterPanel.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.pv168.project.ui.panels.filter;
+
+import cz.muni.fi.pv168.project.ui.filters.IngredientFilter;
+import cz.muni.fi.pv168.project.ui.filters.Range;
+import cz.muni.fi.pv168.project.ui.filters.RecipeFilter;
+import cz.muni.fi.pv168.project.ui.panels.IngredientTablePanel;
+import cz.muni.fi.pv168.project.ui.panels.RecipeTablePanel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class IngredientFilterPanel extends JPanel {
+    private final Range nutritionalRange;
+    private final RangePanel nutritionalValuePanel;
+    private JButton filterButton;
+
+    public IngredientFilterPanel(IngredientTablePanel ingredientTablePanel) {
+        BoxLayout bl = new BoxLayout(this, BoxLayout.Y_AXIS);
+        this.setLayout(bl);
+
+        nutritionalRange = new Range(0, 1000000000);
+        nutritionalValuePanel = new RangePanel(nutritionalRange, "Filter ingredients by their nutritional value", "kcal");
+        this.add(nutritionalValuePanel);
+
+        this.filterButton = new JButton("Apply filters");
+        filterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ingredientTablePanel.applyFilter(new IngredientFilter(nutritionalRange));
+                ingredientTablePanel.getTable().revalidate();
+                ingredientTablePanel.getTable().repaint();
+            }
+        });
+        this.add(filterButton);
+    }
+}
