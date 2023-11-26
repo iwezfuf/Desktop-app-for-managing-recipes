cab911e3fef29843bda48eb3c2b2e28540159664
Ondřej Marek <xmarek9@fi.muni.cz>
2023-11-26 14:40:54 +0100

    IngredientTablePanel now extends entity panel with side panel

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/IngredientTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/IngredientTablePanel.java
@@ -4,38 +4,15 @@ import cz.muni.fi.pv168.project.business.model.Ingredient;
 import cz.muni.fi.pv168.project.business.service.validation.Validator;
 import cz.muni.fi.pv168.project.ui.dialog.EntityDialog;
 import cz.muni.fi.pv168.project.ui.model.EntityTableModel;
+import cz.muni.fi.pv168.project.ui.model.EntityTableModelProvider;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.function.Consumer;
 
-public class IngredientTablePanel extends EntityTablePanel<Ingredient> {
-    public IngredientTablePanel(EntityTableModel<Ingredient> entityTableModel, Validator<Ingredient> ingredientValidator, Class<? extends EntityDialog<Ingredient>> ingredientDialog, Consumer<Integer> onSelectionChange, int frameHeight) {
-        super(entityTableModel, Ingredient.class, ingredientValidator, ingredientDialog, onSelectionChange, frameHeight);
+public class IngredientTablePanel extends EntityTablePanelS<Ingredient> {
+    public IngredientTablePanel(EntityTableModel<Ingredient> entityTableModel, Validator<Ingredient> ingredientValidator, Class<? extends EntityDialog<Ingredient>> ingredientDialog, Consumer<Integer> onSelectionChange, EntityTableModelProvider etmp) {
+        super(entityTableModel, Ingredient.class, ingredientValidator, ingredientDialog, onSelectionChange, etmp);
     }
 
-    @Override
-    protected JTable setUpTable(EntityTableModel<Ingredient> entityTableModel) {
-        var table = new JTable(entityTableModel);
-        table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
-        return table;
-    }
-
-    protected JPanel setUpTableWithSidePanel(EntityTableModel<Ingredient> entityTableModel, int frameHeight) {
-
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        JPanel sidePanel = createSidePanel(frameHeight);
-        mainPanel.add(sidePanel, BorderLayout.WEST);
-        var table = new JTable(entityTableModel);
-        table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
-        mainPanel.add(new JScrollPane(table), BorderLayout.CENTER);
-        return mainPanel;
-    }
-
-    private JPanel createSidePanel(int frameHeight) {
-
-        JPanel sidePanel = new JPanel();
-        sidePanel.setPreferredSize(new Dimension(175, frameHeight));
-        return sidePanel;
-    }
 }
