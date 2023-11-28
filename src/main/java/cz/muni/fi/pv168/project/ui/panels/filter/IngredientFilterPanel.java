db0adc1714744b926b4c3ff5600fa309e92ea28c
Ondřej Marek <xmarek9@fi.muni.cz>
2023-11-28 17:18:47 +0100

    Add reset filter buttons

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/filter/IngredientFilterPanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/filter/IngredientFilterPanel.java
@@ -7,13 +7,15 @@ import cz.muni.fi.pv168.project.ui.panels.IngredientTablePanel;
 import cz.muni.fi.pv168.project.ui.panels.RecipeTablePanel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class IngredientFilterPanel extends JPanel {
-    private final Range nutritionalRange;
+    private Range nutritionalRange;
     private final RangePanel nutritionalValuePanel;
     private JButton filterButton;
+    private JButton resetFilterButton;
 
     public IngredientFilterPanel(IngredientTablePanel ingredientTablePanel) {
         BoxLayout bl = new BoxLayout(this, BoxLayout.Y_AXIS);
@@ -23,12 +25,37 @@ public class IngredientFilterPanel extends JPanel {
         nutritionalValuePanel = new RangePanel(nutritionalRange, "Filter ingredients by their nutritional value", "kcal");
         this.add(nutritionalValuePanel);
 
+        JPanel filterBtnPanel = new JPanel();
+        BoxLayout bl2 = new BoxLayout(filterBtnPanel, BoxLayout.X_AXIS);
+        filterBtnPanel.setLayout(bl2);
         this.filterButton = new JButton("Apply filters");
         filterButton.addActionListener(e -> {
             ingredientTablePanel.applyFilter(new IngredientFilter(nutritionalRange));
             ingredientTablePanel.getTable().revalidate();
             ingredientTablePanel.getTable().repaint();
         });
-        this.add(filterButton);
+        filterBtnPanel.add(filterButton);
+
+        filterBtnPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        this.resetFilterButton = new JButton("Reset filters");
+        resetFilterButton.addActionListener(e -> {
+            ingredientTablePanel.cancelFilter();
+            this.clearDialog();
+            ingredientTablePanel.refresh();
+            ingredientTablePanel.revalidate();
+            ingredientTablePanel.repaint();
+            ingredientTablePanel.getTable().revalidate();
+            ingredientTablePanel.getTable().repaint();
+        });
+        filterBtnPanel.add(resetFilterButton);
+        this.add(filterBtnPanel);
+
+        this.add(Box.createRigidArea(new Dimension(0, 40)));
+    }
+
+    public void clearDialog() {
+        this.nutritionalRange = new Range(0, 1000000000);
+        nutritionalValuePanel.clearPanel();
     }
 }
--- src/main/java/cz/muni/fi/pv168/project/ui/panels/filter/RecipeFilterPanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/filter/RecipeFilterPanel.java
@@ -2,24 +2,22 @@ package cz.muni.fi.pv168.project.ui.panels.filter;
 
 import cz.muni.fi.pv168.project.business.model.Ingredient;
 import cz.muni.fi.pv168.project.business.model.RecipeCategory;
-import cz.muni.fi.pv168.project.ui.filters.RecipeFilter;
-import cz.muni.fi.pv168.project.wiring.EntityTableModelProvider;
-import cz.muni.fi.pv168.project.ui.panels.RecipeTablePanel;
 import cz.muni.fi.pv168.project.ui.filters.Range;
+import cz.muni.fi.pv168.project.ui.filters.RecipeFilter;
+import cz.muni.fi.pv168.project.ui.panels.RecipeTablePanel;
+import cz.muni.fi.pv168.project.wiring.EntityTableModelProvider;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
+import java.awt.*;
 import java.util.HashSet;
 import java.util.Set;
 
-import java.awt.Component;
-import java.awt.Container;
-
 /**
- * Represents a dialog for configuring and applying filters to recipes.
- *
- * This dialog allows users to specify filter criteria such as ingredients, recipe categories,
+ * Represents a panel for configuring and applying filters to recipes.
+ * <p>
+ * This panel allows users to specify filter criteria such as ingredients, recipe categories,
  * and desired calorie ranges.
  *
  * @author Marek Eibel
@@ -37,6 +35,7 @@ public class RecipeFilterPanel extends JPanel {
     private RangePanel prepTimeRangePanel;
 
     private JButton filterButton;
+    private JButton resetFilterButton;
 
     /**
      * Creates new RecipeFilterDialog object.
@@ -49,34 +48,55 @@ public class RecipeFilterPanel extends JPanel {
 
         this.ingredientsSet = new HashSet<>();
         this.ingredientsFilterPanel = new FilterPanel<>(ingredientsSet, etmp.getIngredientTableModel().getEntities(), "ingredient", "ingredients");
-        JButton arrowBtn = getButtonSubComponent(ingredientsFilterPanel.getComboBox());
-        arrowBtn.addActionListener(new ActionListener() {
+        ingredientsFilterPanel.getComboBox().addPopupMenuListener(new PopupMenuListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
                 ingredientsFilterPanel.fillComboBox(etmp.getIngredientTableModel().getEntities());
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent e) {
+
             }
         });
         this.add(ingredientsFilterPanel);
 
         this.recipeCategoriesSet = new HashSet<>();
         this.recipeCategoriesFilterPanel = new FilterPanel<>(recipeCategoriesSet, etmp.getRecipeCategoryTableModel().getEntities(), "recipe category", "recipe categories");
-        JButton arrowBtn2 = getButtonSubComponent(recipeCategoriesFilterPanel.getComboBox());
-        arrowBtn2.addActionListener(new ActionListener() {
+        recipeCategoriesFilterPanel.getComboBox().addPopupMenuListener(new PopupMenuListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
                 recipeCategoriesFilterPanel.fillComboBox(etmp.getRecipeCategoryTableModel().getEntities());
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent e) {
+
             }
         });
         this.add(recipeCategoriesFilterPanel);
 
         this.nutritionalRange = new Range(0, 1000000000);
-        this.nutritionalValueRangePanel = new RangePanel(nutritionalRange, "Apply filtering by nutritional value", "kcal");
+        this.nutritionalValueRangePanel = new RangePanel(nutritionalRange, "Nutritional value:", "kcal");
         this.add(nutritionalValueRangePanel);
 
         this.prepTimeRange = new Range(0, 1000000000);
-        this.prepTimeRangePanel = new RangePanel(prepTimeRange, "Apply filtering by preparation time", "min.");
+        this.prepTimeRangePanel = new RangePanel(prepTimeRange, "Preparation time:", "min.");
         this.add(prepTimeRangePanel);
 
+        JPanel filterBtnPanel = new JPanel();
+        BoxLayout bl2 = new BoxLayout(filterBtnPanel, BoxLayout.X_AXIS);
+        filterBtnPanel.setLayout(bl2);
         this.filterButton = new JButton("Apply filters");
         filterButton.addActionListener(e -> {
             recipeTablePanel.applyFilter(new RecipeFilter(ingredientsSet, recipeCategoriesSet, nutritionalRange, prepTimeRange));
@@ -86,20 +106,34 @@ public class RecipeFilterPanel extends JPanel {
             recipeTablePanel.getTable().revalidate();
             recipeTablePanel.getTable().repaint();
         });
-        this.add(filterButton);
+        filterBtnPanel.add(filterButton);
+
+        filterBtnPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        this.resetFilterButton = new JButton("Reset filters");
+        resetFilterButton.addActionListener(e -> {
+            recipeTablePanel.cancelFilter();
+            this.clearDialog();
+            recipeTablePanel.refresh();
+            recipeTablePanel.revalidate();
+            recipeTablePanel.repaint();
+            recipeTablePanel.getTable().revalidate();
+            recipeTablePanel.getTable().repaint();
+        });
+        filterBtnPanel.add(resetFilterButton);
+        this.add(filterBtnPanel);
+
+        this.add(Box.createRigidArea(new Dimension(0, 40)));
     }
 
-    private JButton getButtonSubComponent(Container container) {
-        if (container instanceof JButton) {
-            return (JButton) container;
-        } else {
-            Component[] components = container.getComponents();
-            for (Component component : components) {
-                if (component instanceof Container) {
-                    return getButtonSubComponent((Container)component);
-                }
-            }
-        }
-        return null;
+    private void clearDialog() {
+        ingredientsSet.clear();
+        recipeCategoriesSet.clear();
+        this.nutritionalRange = new Range(0, 1000000000);
+        this.prepTimeRange = new Range(0, 1000000000);
+        this.ingredientsFilterPanel.clearPanel();
+        this.recipeCategoriesFilterPanel.clearPanel();
+        this.nutritionalValueRangePanel.clearPanel();
+        this.prepTimeRangePanel.clearPanel();
     }
 }
