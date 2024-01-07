30c5fe01adea1df4a686850b9d5a2ae5fb45be3b
Marek Eibel <536376@mail.muni.cz>
2024-01-07 11:41:10 +0100

    Add new class AsyncGenericExportService that takes care of asynchronous export.

---
+++ src/main/java/cz/muni/fi/pv168/project/business/service/export/AsyncGenericExportService.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pv168.project.business.service.export;
+
+import javax.swing.*;
+
+/**
+ * Generic asynchronous export service.
+ *
+ * @author Marek Eibel
+ */
+public class AsyncGenericExportService extends SwingWorker<Void, Void> {
+
+    private final ExportService exportService;
+    private final String filePath;
+
+    public AsyncGenericExportService(ExportService exportService, String filePath) {
+        this.exportService = exportService;
+        this.filePath = filePath;
+    }
+
+    @Override
+    protected Void doInBackground() {
+        exportService.exportData(filePath);
+        return null;
+    }
+
+    @Override
+    protected void done() {
+        JOptionPane.showMessageDialog(null, "Export has successfully finished.");
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+}
